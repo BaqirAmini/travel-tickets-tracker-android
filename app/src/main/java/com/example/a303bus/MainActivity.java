5dee89bee5c8119c9a61ea8aa5c230f6e2e3f904
Engineer Baqir Amini <baqir.amini01@gmail.com>
2022-09-30 15:41:51 +0430

    Complete User Login and Profile.

--- app/src/main/java/com/example/a303bus/MainActivity.java
+++ app/src/main/java/com/example/a303bus/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.a303bus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.annotation.SuppressLint;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,8 +93,21 @@ public class MainActivity extends AppCompatActivity {
 
             if (cursor.getCount() > 0) {
                 String user_role = cursor.getString(cursor.getColumnIndexOrThrow("role"));
-                Intent intent = new Intent(MainActivity.this, profileFragment.class);
-                intent.putExtra("user_role", user_role);
+                String user_phone = cursor.getString(cursor.getColumnIndexOrThrow("phone"));
+                String user_name = cursor.getString(cursor.getColumnIndexOrThrow("user_name"));
+                String user_email = cursor.getString(cursor.getColumnIndexOrThrow("email"));
+
+                profileFragment pf = new profileFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("user_role", user_role);
+                bundle.putString("user_name", user_name);
+                bundle.putString("user_email", user_email);
+                bundle.putString("user_phone", user_phone);
+                pf.setArguments(bundle);
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frag_profile, pf);
+                ft.addToBackStack(null);
+                ft.commit();
             } else {
                 Toast.makeText(this, "Sorry, wrong email or password!", Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/com/example/a303bus/profileFragment.java
+++ app/src/main/java/com/example/a303bus/profileFragment.java
@@ -1,5 +1,8 @@
 package com.example.a303bus;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 public class profileFragment extends Fragment {
     TextView name, email, phone;
@@ -23,12 +28,18 @@ public class profileFragment extends Fragment {
         name = (TextView) view.findViewById(R.id.txtname);
         email = (TextView) view.findViewById(R.id.txtemail);
         phone = (TextView) view.findViewById(R.id.txtphone);
-        logout=(Button) view.findViewById(R.id.logout3);
+        logout = (Button) view.findViewById(R.id.logout3);
+
+        Bundle bundle = this.getArguments();
+        assert bundle != null;
+        name.setText(bundle.getString("user_name"));
+        email.setText(bundle.getString("user_email"));
+        phone.setText(bundle.getString("user_phone"));
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+               System.exit(0);
             }
         });
 
