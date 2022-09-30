e1ca8c9d934bbdedb184cec2303746c654fe06a8
Engineer Baqir Amini <baqir.amini01@gmail.com>
2022-09-30 14:35:46 +0430

    Complete User Login part.

--- app/src/main/java/com/example/a303bus/MainActivity.java
+++ app/src/main/java/com/example/a303bus/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.a303bus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edemail,edpassword;
+    EditText edemail, edpassword;
     Button btnlogin;
-    String email,password;
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +30,19 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
-        edemail=findViewById(R.id.edemail);
-        edpassword=findViewById(R.id.edpass);
+        edemail = findViewById(R.id.edemail);
+        edpassword = findViewById(R.id.edpass);
 
-        TextView signup=findViewById(R.id.signup);
-        TextView txtforgot=findViewById(R.id.forgotpss);
+        TextView signup = findViewById(R.id.signup);
+        TextView txtforgot = findViewById(R.id.forgotpss);
 
 
-        btnlogin=findViewById(R.id.btnlogin);
+        btnlogin = findViewById(R.id.btnlogin);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,singup.class);
+                Intent i = new Intent(MainActivity.this, singup.class);
                 startActivity(i);
             }
         });
@@ -42,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         txtforgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,forgotpassword.class);
+                Intent i = new Intent(MainActivity.this, forgotpassword.class);
                 startActivity(i);
                 finish();
             }
@@ -51,13 +59,47 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                login();
+                onValidate();
             }
         });
 
     }
 
-    public void login() {
+    //    Validate to not allow click the log in button without values.
+    public void onValidate() {
+        if (edemail.getText().toString().isEmpty()) {
+            edemail.setError("Email field cannot be blank.");
+
+        }
+
+        if (edpassword.getText().toString().isEmpty()) {
+            edpassword.setError("Password field cannot be blank.");
+        }
+
+//        Check to see
+        if (!edemail.getText().toString().isEmpty() && !edpassword.getText().toString().isEmpty()) {
+            String email = edemail.getText().toString();
+            String pwd = edpassword.getText().toString();
+
+            if (Build.VERSION.SDK_INT >= 23) {
+                ExternalStoragePermission.verifyStoragePermissions(this);
+            }
+            File inStorage = getBaseContext().getExternalFilesDir(null);
+            String desPath = inStorage.getAbsolutePath() + "/bus303/database/";
+
+            SQLiteDatabase db = SQLiteDatabase.openDatabase(desPath + "/303bus_db.sqlite", null, 0);
+            Cursor cursor = db.rawQuery("SELECT * FROM users WHERE email = '" + email + "' AND password = '" + pwd + "'", null);
+            cursor.moveToFirst();
+
+            if (cursor.getCount() > 0) {
+                String user_role = cursor.getString(cursor.getColumnIndexOrThrow("role"));
+                Intent intent = new Intent(MainActivity.this, profileFragment.class);
+                intent.putExtra("user_role", user_role);
+            } else {
+                Toast.makeText(this, "Sorry, wrong email or password!", Toast.LENGTH_SHORT).show();
+
+            }
+        }
 
     }
 }
